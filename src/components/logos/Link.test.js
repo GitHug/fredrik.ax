27a2c000fe759af5f16f9cb890a769f8939a38a5
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-11-05 14:55:09 +0000

    Fix unit test

--- src/components/logos/Link.test.js
+++ src/components/logos/Link.test.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ImageLink from './Link';
+import Link from './Link';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ImageLink url={'url'} image={'image'} alt={'alt'} />, div);
+  ReactDOM.render(<Link url={'url'} content={<div>test</div>} />, div);
 });
 
 it('renders an anchor tag containing an url and an image', () => {
-  const component = renderer.create(
-    <ImageLink url={'url'} content={'content'} />
-  );
+  const component = renderer.create(<Link url={'url'} content={'content'} />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
   expect(tree.props.href).toBe('url');
-  expect(tree.children[0].type).toBe('img');
-  expect(tree.children[0].props.content).toBe('content');
+  expect(tree.children[0]).toBe('content');
 });
