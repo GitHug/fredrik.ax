161d065e291b8800080bab669e452c7c14b6a37d
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-11-15 22:02:40 +0000

    Making API calls work

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
 import './LoginForm.css';
+import axios from 'axios';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = { value: '' };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  handleChange(event) {
+  onChange = event => {
     this.setState({ value: event.target.value });
-  }
+  };
 
-  handleSubmit(event) {
+  onSubmit = event => {
     window.alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
-  }
+
+    const { value } = this.state;
+
+    axios
+      .post(
+        (process.env.REACT_APP_BACKEND_HOST || 'http://localhost:8080') +
+          '/login',
+        { value }
+      )
+      .then(response => {
+        console.log(response, 'Signature added!');
+      })
+      .catch(err => {
+        console.log(err, 'Signature not added, try again');
+      });
+  };
 
   render() {
     return (
-      <form className={'LoginForm'} onSubmit={this.handleSubmit}>
+      <form className={'LoginForm'} onSubmit={this.onSubmit}>
         <div class="form-group">
           <label for="psw">Password</label>
           <input
@@ -30,7 +46,7 @@ class LoginForm extends Component {
             id="psw"
             placeholder="Password"
             value={this.state.value}
-            onChange={this.handleChange}
+            onChange={this.onChange}
           />
         </div>
         <button type="submit" class="btn btn-primary">
